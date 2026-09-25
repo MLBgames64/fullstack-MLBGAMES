a31f55d58a16c77c32e7a2d21d8326587414177a
Luis-romero5212 <lu.romerol@duocuc.cl>
2026-09-25 17:24:26 -0300

    lr-inicio-script

---
+++ script.js
@@ -0,0 +1,13 @@
+//redirigir al iniciar secion al index.html
+const formulario = document.getElementById (formlogin);
+
+formulario.addEventListener("subit",function(evento){
+    evento.preventDefault();
+
+    const correo = document.getElementById("correo").value;
+    const password = document.getElementById("password").value;
+
+    window.location.href="index.html";
+});
+
+const
